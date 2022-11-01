22646b8120fe73afa48e5382cd98495fb0f3c11d
Mamta Aithani <mamta@santosh>
2022-11-01 05:22:38 -0500

    routers

---
+++ crwn-clothing/src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,24 @@
+import { Link, Outlet } from 'react-router-dom';
+import './navigation.styles.scss';
+
+import {ReactComponent as CrwnLogo } from '../../assets/crown.svg';
+
+const Navigation = ()  => {
+    return (
+            <div>
+                <div className="navigation">      
+                    <Link className='logo-container' to='/'>
+                       <CrwnLogo className='logo' />
+                    </Link>
+                    <div className="nav-links-container">
+                        <Link className='nav-link' to='/'>Home</Link>
+                        <Link className='nav-link' to='/Shop'>Shop</Link>
+                    </div>            
+                  
+                </div>    
+                <Outlet/>
+            </div>
+        )     
+};
+    
+export default Navigation;
